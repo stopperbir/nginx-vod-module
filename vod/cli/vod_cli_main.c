67d814b8d07b79facaa527155aa80666c55d7c26
stopperbir <96837517+stopperbir@users.noreply.github.com>
2026-02-08 17:54:37 +0300

    test

--- vod/cli/vod_cli_main.c
+++ vod/cli/vod_cli_main.c
@@ -1,5 +1,5 @@
 // gcc -Wall -g -o repack adts_encoder.c buffer_filter.c mp4_parser.c mp4_to_annexb.c mpegts_encoder.c repackTs.c vod_array.c muxer.c read_cache.c
-// ./repack /opt/kaltura/app/alpha/web/repack.jpeg 0 10
+// ./repack /opt/kaltura/app/alpha/web/repack.js 0 10
 
 #include <sys/stat.h>
 #include <unistd.h>
